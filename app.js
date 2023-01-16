8ef3ebb8ec509765d562be43ad88d13fd1a8dcac
A5HU705H <cse210001005@iiti.ac.in>
2023-01-16 21:19:03 +0530

    init

--- app.js
+++ app.js
@@ -1,191 +1,173 @@
-const grid= document.querySelector('div.grid');
-const body= document.querySelector('body');
+const grid = document.querySelector('div.grid');
+const body = document.querySelector('body');
+const update = document.querySelector('.upd');
+const range = document.querySelector('.ip');
+const shade = document.querySelector('.shade');
+const rainbow = document.querySelector('.rando');
+const erase = document.querySelector('.eraser');
+grid.addEventListener('mousedown', () => { drag = false });
+grid.addEventListener('mouseup', () => { drag = true });
+grid.addEventListener('mouseleave', ()=>{drag= true;})
+let black = false;
+let shading = false;
+let Rainbow = false;
+let eraze= false;
+let drag = true;
+// var rangeInput = document.getElementsByClassName("ip");
 
-const update= document.querySelector('.upd');
-const range= document.querySelector('.ip');
-const shade= document.querySelector('.shade');
-const rainbow= document.querySelector('.rando');
-const erase= document.querySelector('.eraser');
+function addShading(block){
+    if (!drag && !eraze && shading) { block.style.backgroundColor = 'black'; block.style.opacity = `${parseFloat(block.style.opacity) + 0.1}`}
+}
 
-let black=false;
-let shading=false;
-let Rainbow=false;
+range.addEventListener('mouseup', function() {
+    let a = range.value;
+    grid.textContent = '';
+    for (let i = 0; i < a; i++) {
+        const row = document.createElement(`div`);
+        row.classList.add('row');
+        row.style.height = `${500 / a}` + 'px';
+        row.style.width = '500px';
+        for (let j = 0; j < a; j++) {
+            const block = document.createElement(`div`);
+            const border = document.createElement('div');
+            block.classList.add('block');
+            block.style.height = `${500 / (a)}` + 'px';
+            block.style.width = `${500 / (a)}` + 'px';
+            if(shading){block.style.opacity = '0';}
+            block.textContent = ' ';
+            row.appendChild(block);
+        }
+        grid.appendChild(row);
+    }
 
-erase.addEventListener('click', ()=>{
-    // let a=range.value;
-    // grid.textContent = '';
-    // for(let i=0; i<a; i++){
-    //     const row= document.createElement(`div`);
-    //     row.classList.add('row');
-    //     row.style.height=`${500/a}`+'px';
-    //     row.style.width= '500px';
-    //     for(let j=0; j<a; j++){
-    //         const block=document.createElement(`div`);
-    //         block.classList.add('block');
-    //         block.style.height= `${500/(a)}`+'px';
-    //         block.style.width= `${500/(a)}`+'px';
-    //         block.textContent=' ';
-    //         row.appendChild(block);
-    //     }
-    //     grid.appendChild(row);
-    // }
-    const blocks= document.querySelectorAll('.block');
-    let drag = true;
+    const blocks = document.querySelectorAll('.block');
+    if(black){
+        blocks.forEach((block) => block.addEventListener('mouseenter', () => {
+            if (!drag && !eraze) { block.style.backgroundColor = 'black';  }
+    
+        }));
+    }else if(shading){
+        blocks.forEach((block) => block.addEventListener('mouseenter', () => {
+            if(!drag && !eraze){block.style.backgroundColor = 'black'; block.style.opacity = `0.0`; }  
+        }));
+    }else{
+        blocks.forEach((block) => block.addEventListener('mouseenter', () => {
+            if (!drag && !eraze) { block.style.backgroundColor = `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 1} )`; }
+    
+        }));
+    }
+});
 
-    grid.addEventListener('mousedown', () => {drag = false});
-    grid.addEventListener('mouseup', () => {drag = true});
-
-    blocks.forEach((block) => block.addEventListener('mousemove', ()=>{
-    if(!drag){block.style.backgroundColor = 'none'; }
+erase.addEventListener('click', () => {
+    if(!eraze){eraze=true;}
+    const blocks = document.querySelectorAll('.block');
+    blocks.forEach((block) => block.addEventListener('mouseenter', () => {
+        if (!drag && eraze) { if(shading){block.style.opacity=`0.0`;}else{block.style.backgroundColor='transparent';} }
     }));
 })
 
-rainbow.addEventListener('click', ()=>{
-    if(Rainbow){
-        const blocks= document.querySelectorAll('.block');
-        let drag = true;
-
-        grid.addEventListener('mousedown', () => {drag = false});
-        grid.addEventListener('mouseup', () => {drag = true});
-
-        blocks.forEach((block) => block.addEventListener('mousemove', ()=>{
-        if(!drag){block.style.backgroundColor = `rgba(${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255}, ${Math.random()*1} )`; }
+rainbow.addEventListener('click', () => {
+    const blocks = document.querySelectorAll('.block');
+    eraze= false;
+    if (Rainbow) {
+        
+        blocks.forEach((block) => block.addEventListener('mouseenter', () => {
+            if (!drag && !eraze) { block.style.backgroundColor = `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 1} )`; }
         }));
         return;
-    }else{
-        Rainbow=true; shading=false; black=false;
+    } else {
+        Rainbow = true; shading = false; black = false;
     }
-    let a=range.value;
+    let a = range.value;
     grid.textContent = '';
-    for(let i=0; i<a; i++){
-        const row= document.createElement(`div`);
+    for (let i = 0; i < a; i++) {
+        const row = document.createElement(`div`);
         row.classList.add('row');
-        row.style.height=`${500/a}`+'px';
-        row.style.width= '500px';
-        for(let j=0; j<a; j++){
-            const block=document.createElement(`div`);
+        row.style.height = `${500 / a}` + 'px';
+        row.style.width = '500px';
+        for (let j = 0; j < a; j++) {
+            const block = document.createElement(`div`);
             block.classList.add('block');
-            block.style.height= `${500/(a)}`+'px';
-            block.style.width= `${500/(a)}`+'px';
-            block.textContent=' ';
+            block.style.height = `${500 / (a)}` + 'px';
+            block.style.width = `${500 / (a)}` + 'px';
+            block.textContent = ' ';
             row.appendChild(block);
         }
         grid.appendChild(row);
     }
-    const blocks= document.querySelectorAll('.block');
-    let drag = true;
-
-    grid.addEventListener('mousedown', () => {drag = false});
-    grid.addEventListener('mouseup', () => {drag = true});
-
-    blocks.forEach((block) => block.addEventListener('mousemove', ()=>{
-    if(!drag){block.style.backgroundColor = `rgba(${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255}, ${Math.random()*1} )`; }
+    blocks.forEach((block) => block.addEventListener('mouseenter', () => {
+        if (!drag && !eraze) { block.style.backgroundColor = `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 1} )`; }
     }));
 })
 
-shade.addEventListener('click', ()=>{
-    if(shading){
-        const blocks= document.querySelectorAll('.block');
-    let drag = true;
-
-    grid.addEventListener('mousedown', () => {drag = false});
-    grid.addEventListener('mouseup', () => {drag = true});
-
-    
-
-    blocks.forEach((block) => block.addEventListener('mousemove', ()=>{
-    if(!drag){block.style.backgroundColor = 'black'; block.style.opacity= `${parseFloat(block.style.opacity)+0.1}`;}
-    }));
+shade.addEventListener('click', () => {
+    const blocks = document.querySelectorAll('.block');
+    eraze= false;
+    if (shading) {
+        blocks.forEach((block) => block.removeEventListener('mouseenter', addShading(block)));
+        blocks.forEach((block) => block.addEventListener('mouseenter', addShading(block)));
         return;
-    }else{shading=true; Rainbow=false; black=false;}
-    let a=range.value;
+    } else { shading = true; Rainbow = false; black = false; }
+    let a = range.value;
     grid.textContent = '';
-    for(let i=0; i<a; i++){
-        const row= document.createElement(`div`);
+    for (let i = 0; i < a; i++) {
+        const row = document.createElement(`div`);
         row.classList.add('row');
-        row.style.height=`${500/a}`+'px';
-        row.style.width= '500px';
-        for(let j=0; j<a; j++){
-            const block=document.createElement(`div`);
-            const border= document.createElement('div');
-            border.style.height= `${500/(a)}`+'px';
-            border.style.width= `${500/(a)}`+'px';
-            border.style.border= '0.01px solid black';
+        row.style.height = `${500 / a}` + 'px';
+        row.style.width = '500px';
+        for (let j = 0; j < a; j++) {
+            const block = document.createElement(`div`);
+            const border = document.createElement('div');
             block.classList.add('block');
-            block.style.height= `${500/(a)}`+'px';
-            block.style.width= `${500/(a)}`+'px';
-            block.style.opacity= '0';
-            block.textContent=' ';
-            border.appendChild(block);
-            row.appendChild(border);
-        }
-        grid.appendChild(row);
-    }
-    const blocks= document.querySelectorAll('.block');
-    let drag = true;
-
-    grid.addEventListener('mousedown', () => {drag = false});
-    grid.addEventListener('mouseup', () => {drag = true});
-
-    
-
-    blocks.forEach((block) => block.addEventListener('mousemove', ()=>{
-    if(!drag){block.style.backgroundColor = 'black'; block.style.opacity= `${parseFloat(block.style.opacity)+0.1}`;}
-    }));
-})
-
-update.addEventListener('click', ()=>{
-    if(black){const blocks= document.querySelectorAll('.block');
-    let drag = true;
-
-    grid.addEventListener('mousedown', () => {drag = false});
-    grid.addEventListener('mouseup', () => {drag = true});
-
-    blocks.forEach((block) => block.addEventListener('mousemove', ()=>{
-    if(!drag){block.style.backgroundColor = 'black'; }
-    }));
-    return;
-    }else{
-        black=true; shading=false; Rainbow=false;
-    }   
-    let a=range.value;
-    grid.textContent = '';
-    for(let i=0; i<a; i++){
-        const row= document.createElement(`div`);
-        row.classList.add('row');
-        row.style.height=`${500/a}`+'px';
-        row.style.width= '500px';
-        for(let j=0; j<a; j++){
-            const block=document.createElement(`div`);
-            block.classList.add('block');
-            block.style.height= `${500/(a)}`+'px';
-            block.style.width= `${500/(a)}`+'px';
-            block.textContent=' ';
+            block.style.height = `${500 / (a)}` + 'px';
+            block.style.width = `${500 / (a)}` + 'px';
+            block.style.opacity = `0.0`;
+            block.textContent = ' ';
             row.appendChild(block);
         }
         grid.appendChild(row);
     }
-    const blocks= document.querySelectorAll('.block');
-    let drag = true;
+    blocks.forEach((block) => block.addEventListener('mouseenter', addShading(block)));
+})
 
-    grid.addEventListener('mousedown', () => {drag = false});
-    grid.addEventListener('mouseup', () => {drag = true});
+update.addEventListener('click', () => {
+    const blocks = document.querySelectorAll('.block');
+    eraze=false;
+    if (black) {
+    
 
-    blocks.forEach((block) => block.addEventListener('mousemove', ()=>{
-    if(!drag){block.style.backgroundColor = 'black'; }
+        blocks.forEach((block) => block.addEventListener('mouseenter', () => {
+            if (!drag && !eraze) { block.style.backgroundColor = 'black'; }
+        }));
+        return;
+    } else {
+        black = true; shading = false; Rainbow = false;
+    }
+    let a = range.value;
+    grid.textContent = '';
+    for (let i = 0; i < a; i++) {
+        const row = document.createElement(`div`);
+        row.classList.add('row');
+        row.style.height = `${500 / a}` + 'px';
+        row.style.width = '500px';
+        for (let j = 0; j < a; j++) {
+            const block = document.createElement(`div`);
+            block.classList.add('block');
+            block.style.height = `${500 / (a)}` + 'px';
+            block.style.width = `${500 / (a)}` + 'px';
+            block.textContent = ' ';
+            row.appendChild(block);
+        }
+        grid.appendChild(row);
+    }
+    blocks.forEach((block) => block.addEventListener('mouseenter', () => {
+        if (!drag && !eraze) { block.style.backgroundColor = 'black'; }
     }));
 });
 
-
-const blocks= document.querySelectorAll('.block');
-    let drag = true;
-
-    grid.addEventListener('mousedown', () => {drag = false});
-    grid.addEventListener('mouseup', () => {drag = true});
-
-
-    blocks.forEach((block) => block.addEventListener('mousemove', ()=>{
-    if(!drag){block.style.backgroundColor = 'black';}
-    }));
+const blocks = document.querySelectorAll('.block');
+blocks.forEach((block) => block.addEventListener('mouseenter', () => {
+    if (!drag && !eraze && !shading && !Rainbow && !eraze && !black) { block.style.backgroundColor = 'black'; }
+}));
 
 
